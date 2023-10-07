c1cdc28d0d452c6056dae040b8347644db992e90
Sachin Kumar Sharma <89272sachin1@gmail.com>
2023-10-07 20:12:52 +0000

    DataTypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS Code as Newer Version
+// alert( 3+3 ) We are Using Nodejs Not Browser
+
+console.log(3+3)
+console.log("Sachin")
+
+let name = "sachin"
+let age = 18
+let isLoggedIn = true  // false
+
+// Number => 2 to power 53
+// bigInt => 
+// strings => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// Object
+
+console.log(typeof null)
