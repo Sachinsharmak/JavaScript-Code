7184e2cf3e26be1fd43906c4ae212dba4250efae
Sachin Kumar Sharma <89272sachin1@gmail.com>
2023-10-08 19:37:02 +0000

    Conversions

---
+++ 01_Basics/03_conversionofData.js
@@ -0,0 +1,29 @@
+ /* "33" -> 33
+"33abc" => NaN
+ "true" => 1, false => 0
+ 1 => true, 0 => false
+ "" => false
+ "as" => true
+*/
+
+// let score = "sachin"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = "Sachin"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
