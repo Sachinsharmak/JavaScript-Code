a6209065146a58d7af4cc70558bac47efe0e2f26
Sachin Kumar Sharma <89272sachin1@gmail.com>
2023-10-10 19:01:13 +0000

    Data Types

--- 01_Basics/03_conversionofData.js
+++ 01_Basics/03_conversionofData.js
@@ -22,8 +22,45 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// **************************** OPERATIONS *************************
+// let value = 3
+// let negVal = -value
+// console.log(negVal);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "sachin"
+// let str2 = "sharma"
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 5 %3)
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+let gameCounter1 = 100
+gameCounter++
+++gameCounter1
+console.log(gameCounter)
+console.log(gameCounter1)
